f601cc7b3206d7cd8433cdfa2a5b24f0fc73fddf
DiegoPerez <diegoperezw@gmail.com>
2010-11-24 15:07:01 +0100

    packaging script changes

--- Deb/createclaudiac.sh
+++ Deb/createclaudiac.sh
@@ -7,7 +7,7 @@ cd  $CLAUDIAWORKDIR/claudia-client/target/
 rm -fr $CLAUDIAWORKDIR/Deb/stratuslab-claudia-client/opt/claudia/*
 
 mkdir -p $CLAUDIAWORKDIR/Deb/stratuslab-claudia-client/opt/claudia/
-cp -R $CLAUDIAWORKDIR/clotho/target/claudia-client-0.1.1-environment.dir/ClaudiaClient/* $CLAUDIAWORKDIR/Deb/stratuslab-claudia-client/opt/claudia/
+cp -R $CLAUDIAWORKDIR/claudia-client/target/claudia-client-0.1.1-environment.dir/ClaudiaClient/* $CLAUDIAWORKDIR/Deb/stratuslab-claudia-client/opt/claudia/
 
 cd $CLAUDIAWORKDIR/Deb
 mv $CLAUDIAWORKDIR/Deb/stratuslab-claudia-client/opt/claudia/lib/* $CLAUDIAWORKDIR/Deb/stratuslab-claudia-lib/opt/claudia/lib/
