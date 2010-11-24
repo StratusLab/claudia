baabe0161707ceee3b2e35d24c69c32e69a07fb2
DiegoPerez <diegoperezw@gmail.com>
2010-11-24 15:45:17 +0100

    packaging script changes

--- Deb/createall.sh
+++ Deb/createall.sh
@@ -2,14 +2,6 @@
 
 rm -fr stratuslab-claudia-lib/opt/claudia/lib/*
 
-
-#mv  stratuslab-claudia-lib.deb finaldeb/stratuslab-claudia-lib-0.1-3.deb
-#mv  stratuslab-jms2rest.deb finaldeb/stratuslab-jms2rest-0.1-1.deb
-#mv  stratuslab-ovf-manager.deb finaldeb/stratuslab-ovf-manager-0.1-1.deb
-#mv  stratuslab-wasup.deb finaldeb/stratuslab-wasup-0.1-1.deb
-#mv  stratuslab-ezweb.deb finaldeb/stratuslab-ezweb-0.1-1.deb
-#mv  stratuslab-claudia.deb finaldeb/stratuslab-claudia-0.1-2.deb
-
 ./createclotho.sh
 ./createtcloud.sh
 ./createclaudiac.sh
@@ -18,6 +10,10 @@ rm -fr stratuslab-claudia-lib/opt/claudia/lib/*
 
 dpkg-deb --build stratuslab-claudia-lib
 dpkg-deb --build stratuslab-claudia
+
+mv  stratuslab-claudia-lib.deb  $CLAUDIADEBDIR/stratuslab-claudia-lib-0.1-3.deb
+mv  stratuslab-claudia.deb $CLAUDIADEBDIR/stratuslab-claudia-0.1-2.deb
+
 
 rm -fr stratuslab-clotho/opt
 rm -fr stratuslab-claudia-lib/opt/claudia/lib/*
