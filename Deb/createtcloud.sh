6e0fdb346a3a56ec2ad9bde48042a37b95d18281
DiegoPerez <diegoperezw@gmail.com>
2010-11-24 15:12:12 +0100

    packaging script changes

--- Deb/createtcloud.sh
+++ Deb/createtcloud.sh
@@ -1,29 +1,19 @@
 #!/bin/sh
 echo "------------------------------------------"
-echo "Creando Tcloud server"
+echo "Creating Tcloud-server Package"
 echo "------------------------------------------"
 
 
-cd /home/dperez/Documentos/StratusLabs/git/claudia/tcloud-server/
+cd  $CLAUDIAWORKDIR/tcloud-server/target/
+rm -fr $CLAUDIAWORKDIR/Deb/stratuslab-tcloud-server/opt/claudia/*
 
-mvn clean
-mvn assembly:assembly 
+mkdir -p $CLAUDIAWORKDIR/Deb/stratuslab-tcloud-server/opt/claudia/
+cp -R $CLAUDIAWORKDIR/tcloud-server/target/tcloud-server-0.1.1-environment.dir/tcloud-server/* $CLAUDIAWORKDIR/Deb/stratuslab-tcloud-server/opt/claudia/
 
-cd  /home/dperez/Documentos/StratusLabs/git/claudia/tcloud-server/target/
-cp -f /home/dperez/Documentos/StratusLabs/git/claudia/tcloud-server/target/tcloud-server-0.1.1-deployable.zip /home/dperez/Documentos/StratusLabs/Deb/deployable/tcloud-server-0.2.1-deployable.zip  
-cd /home/dperez/Documentos/StratusLabs/Deb/deployable/
-rm -fr tcloud-server
-unzip tcloud-server-0.2.1-deployable.zip
-cd tcloud-server
-rm -fr /home/dperez/Documentos/StratusLabs/Deb/stratuslab-tcloud-server/opt/claudia/*
-cp -R * /home/dperez/Documentos/StratusLabs/Deb/stratuslab-tcloud-server/opt/claudia/
-
-cd /home/dperez/Documentos/StratusLabs/Deb
-rm /home/dperez/Documentos/StratusLabs/Deb/stratuslab-tcloud-server/opt/claudia/lib/ovf-manager-0.1-SNAPSHOT.jar
-
-./compare.sh stratuslab-tcloud-server/ stratuslab-claudia-lib/
+cd $CLAUDIAWORKDIR/Deb
+mv $CLAUDIAWORKDIR/Deb/stratuslab-tcloud-server/opt/claudia/lib/* $CLAUDIAWORKDIR/Deb/stratuslab-claudia-lib/opt/claudia/lib/
 
 dpkg-deb --build stratuslab-tcloud-server
-mv  stratuslab-tcloud-server.deb finaldeb/stratuslab-tcloud-server-0.1-2.deb
+mv  stratuslab-tcloud-server.deb $CLAUDIADEBDIR/stratuslab-tcloud-server-0.1-2.deb
 
 
