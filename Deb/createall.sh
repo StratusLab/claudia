4a760362b74db546527ac8e9c30e00611187ed23
DiegoPerez <diegoperezw@gmail.com>
2010-11-24 15:12:53 +0100

    packaging script changes

--- Deb/createall.sh
+++ Deb/createall.sh
@@ -22,3 +22,4 @@ dpkg-deb --build stratuslab-claudia
 rm -fr stratuslab-clotho/opt
 rm -fr stratuslab-claudia-lib/opt/claudia/lib/*
 rm -fr stratuslab-claudia-client/opt
+rm -fr stratuslab-tcloud-server/opt
