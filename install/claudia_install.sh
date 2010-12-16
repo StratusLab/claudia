530a483ae52cb4a14a3c4bc599db96dbf13c44ee
DiegoPerez <diegoperezw@gmail.com>
2010-12-16 16:23:18 +0100

    packaging script changes

---
+++ install/claudia_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sho
+
+DEST=$1
+
+ssh  root@$DEST 'yum install stratuslab-clotho'
+ssh  root@$DEST 'yum install stratuslab-tcloud-server'
+ssh  root@$DEST 'yum install stratuslab-claudia-client'
