55bff55869ca95a96dd6559d2db3b5af6a1b2934
DiegoPerez <diegoperezw@gmail.com>
2011-04-21 12:41:59 +0200

    reportclient changes

--- reportclient/src/main/java/com/telefonica/claudia/slm/monitoring/Monitor.java
+++ reportclient/src/main/java/com/telefonica/claudia/slm/monitoring/Monitor.java
@@ -1,11 +1,13 @@
 package com.telefonica.claudia.slm.monitoring;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
@@ -22,7 +24,13 @@ public class Monitor extends Thread {
 	private static final Logger logger = Logger.getLogger(Monitor.class);
 	public static List<String> totalvmlist;
 	private String URL;
-
+	
+	public static final String PATH_TO_PROPERTIES_FILE = "./conf/reportClient.properties";
+	private static final Properties properties = new Properties();
+	private static long vmdiscoverdelay;
+	private static long monitoringdelay;
+	
+	
 	public Monitor(String url){
 		this.URL = url;
 	}
@@ -88,8 +96,9 @@ public class Monitor extends Thread {
 				String measure = (String) iterator2.next();	
 
 				String valuexml=pc.getmeasure(monitor, measure);
+				String measurefqn = monitor+"/"+measure;
 				logger.info(" monitor values: " + monitor+ " "+measure); 
-				pc.sendvalue(valuexml,monitor);
+				pc.sendvalue(valuexml,measurefqn);
 
 			}
 
@@ -97,16 +106,27 @@ public class Monitor extends Thread {
 
 	}
 	public static void generateAllReport() throws IOException {
+		
+		try {
+			properties.load(new FileInputStream(PATH_TO_PROPERTIES_FILE));
+
+			vmdiscoverdelay =  Long.parseLong(properties.getProperty("vmDiscoverDelay"));
+			monitoringdelay =  Long.parseLong(properties.getProperty("monitoringDelay"));
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to load properties from " + PATH_TO_PROPERTIES_FILE);
+		}
+	
+	
 		//long i1 = System.currentTimeMillis();
 		PersistenceClient pc = new PersistenceClient();
 		totalvmlist = pc.getVMs();
 		periodicMonitoring();
 		
 		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-		executor.scheduleAtFixedRate(periodicGetVm,0,180, TimeUnit.SECONDS);
+		executor.scheduleAtFixedRate(periodicGetVm,0,vmdiscoverdelay, TimeUnit.SECONDS);
 		
 		ScheduledExecutorService executor2 = Executors.newSingleThreadScheduledExecutor();
-		executor2.scheduleAtFixedRate(periodicGetMonValues,0,20, TimeUnit.SECONDS);
+		executor2.scheduleAtFixedRate(periodicGetMonValues,0,monitoringdelay, TimeUnit.SECONDS);
 		
 		//long t1 = System.currentTimeMillis() - i1;
 		//logger.debug("generateAllReport Total Time:" + t1 + " miliseg");					
