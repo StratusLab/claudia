9ef518ebe3ce9cb255f9ece2e74203b73ad48d1b
DiegoPerez <diegoperezw@gmail.com>
2011-04-21 05:04:37 +0200

    reportclient changes

--- reportclient/src/main/java/com/telefonica/claudia/slm/monitoring/Monitor.java
+++ reportclient/src/main/java/com/telefonica/claudia/slm/monitoring/Monitor.java
@@ -12,22 +12,50 @@ import org.apache.log4j.Logger;
 import com.telefonica.claudia.slm.monitoring.report.MonitoringRestClient;
 import com.telefonica.claudia.slm.monitoring.report.TypeEntityEnum;
 
+import edu.emory.mathcs.backport.java.util.concurrent.Executors;
+import edu.emory.mathcs.backport.java.util.concurrent.ScheduledExecutorService;
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
 public class Monitor extends Thread {
 
 	private static final MonitoringRestClient client = new MonitoringRestClient();
 	private static final Logger logger = Logger.getLogger(Monitor.class);
-	
+	public static List<String> totalvmlist;
 	private String URL;
-	
+
 	public Monitor(String url){
 		this.URL = url;
 	}
+
+	static Runnable periodicGetVm = new Runnable(){
+		public void run() {
+			PersistenceClient pc = new PersistenceClient();
+			try {
+				totalvmlist = pc.getVMs();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	};
 	
+	static Runnable periodicGetMonValues= new Runnable(){
+		public void run() {
+			PersistenceClient pc = new PersistenceClient();
+			try {
+				periodicMonitoring();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	};
+
 	public void run() {
 		System.out.println("Starting for "+URL);
 		generateReport(URL);
 	}
-	
+
 	private void generateReport(String baseUrl) {
 		Calendar day = new GregorianCalendar();
 		TypeEntityEnum[] typeEntities = TypeEntityEnum.values();
@@ -48,12 +76,10 @@ public class Monitor extends Thread {
 		}
 	}
 
-	public static void generateAllReport() throws IOException {
-		long i1 = System.currentTimeMillis();
+	public static void periodicMonitoring() throws IOException {
+
 		PersistenceClient pc = new PersistenceClient();
-		List<String> list = pc.getVMs();
-		ArrayList<String> monitorlist = pc.findmonitors(list);
-		
+		ArrayList<String> monitorlist = pc.findmonitors(totalvmlist);
 
 		for (Iterator iterator = monitorlist.iterator(); iterator.hasNext();) {
 			String monitor = (String) iterator.next();	
@@ -68,44 +94,41 @@ public class Monitor extends Thread {
 			}
 
 		}
-		/*	List<Monitor> monitorThreads = new ArrayList<Monitor>();
-		for (int i = 0; i < list.size(); i++) {
-			String vmUrl = list.get(i);			
-			logger.debug("VM Url:"+vmUrl);
-			Monitor monitor = new Monitor(vmUrl);
-			monitorThreads.add(monitor);
-			monitor.start();
-		}
-		for (Iterator iterator = monitorThreads.iterator(); iterator.hasNext();) {
-			Monitor monitor = (Monitor) iterator.next();
-			try {
-				monitor.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}*/
 
-		long t1 = System.currentTimeMillis() - i1;
-		logger.debug("generateAllReport Total Time:" + t1 + " miliseg");					
+	}
+	public static void generateAllReport() throws IOException {
+		//long i1 = System.currentTimeMillis();
+		PersistenceClient pc = new PersistenceClient();
+		totalvmlist = pc.getVMs();
+		periodicMonitoring();
+		
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(periodicGetVm,0,180, TimeUnit.SECONDS);
+		
+		ScheduledExecutorService executor2 = Executors.newSingleThreadScheduledExecutor();
+		executor2.scheduleAtFixedRate(periodicGetMonValues,0,20, TimeUnit.SECONDS);
+		
+		//long t1 = System.currentTimeMillis() - i1;
+		//logger.debug("generateAllReport Total Time:" + t1 + " miliseg");					
 	}
 
 	private static void showUsage() {
 		System.out.println("Usage:\n\nmonitor <monitorType> \n\n");
 		System.out.println("\t allmonitoring");
 		System.out.println("\t\tStart monitoring allparameters (Except DiskUsage).\n");
-		
+
 		System.out.println("\t diskusage");
 		System.out.println("\t\tStart monitoring DiskUsage.\n");
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-	 	if (args.length < 1) {
-    		System.out.println("Command not recognized. \n");
-    		showUsage();
-    	}
-	 	else if (args.length==1){
+		if (args.length < 1) {
+			System.out.println("Command not recognized. \n");
+			showUsage();
+		}
+		else if (args.length==1){
 			if (args[0].toLowerCase().equals("allmonitoring") ){
-		/*		NICMonitor nic = new NICMonitor();
+				/*		NICMonitor nic = new NICMonitor();
 				nic.start();*/
 				generateAllReport();
 			}
