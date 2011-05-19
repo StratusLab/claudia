7298b7cd5b2b3a3b43386cd06c5d6191d6e3484c
Diego Pérez Fabado <diegoperezw@gmail.com>
2011-05-19 08:12:14 +0200

    now report client is not started in claudia-start

--- clotho/src/main/config/claudia-startup.sh
+++ clotho/src/main/config/claudia-startup.sh
@@ -2,10 +2,9 @@
 
 /etc/init.d/clothod stop
 /etc/init.d/tcloudd stop
-/etc/init.d/reportclientd stop
 sleep 5
  rm -fr /opt/claudia/ClaudiaDB/
 /etc/init.d/clothod start
 /etc/init.d/tcloudd start
 sleep 7
-/etc/init.d/reportclientd start
+
