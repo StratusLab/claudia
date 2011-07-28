46ad3351e5a5c7cadc33e02297d61b287db9e205
unknown <henar@.hi.inet>
2011-07-28 14:33:21 +0200

    changes in TCloudConstants

--- claudia-commons/src/main/java/com/telefonica/claudia/smi/TCloudConstants.java
+++ claudia-commons/src/main/java/com/telefonica/claudia/smi/TCloudConstants.java
@@ -1,33 +1,23 @@
 /*
-* Claudia Project
-* http://claudia.morfeo-project.org
-*
-* (C) Copyright 2010 Telefonica Investigacion y Desarrollo
-* S.A.Unipersonal (Telefonica I+D)
-*
-* See CREDITS file for info about members and contributors.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the Affero GNU General Public License (AGPL) as 
-* published by the Free Software Foundation; either version 3 of the License, 
-* or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the Affero GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*
-* If you want to use this software an plan to distribute a
-* proprietary application in any way, and you are not licensing and
-* distributing your source code under AGPL, you probably need to
-* purchase a commercial license of the product. Please contact
-* claudia-support@lists.morfeo-project.org for more information.
-*/
+
+  (c) Copyright 2011 Telefonica, I+D. Printed in Spain (Europe). All Righ
+  Reserved.
+
+  The copyright to the software program(s) is property of Telefonica I+D.
+  The program(s) may be used and or copied only with the express written
+  consent of Telefonica I+D or in accordance with the terms and conditions
+  stipulated in the agreement/contract under which the program(s) have
+  been supplied.
+
+  */
 package com.telefonica.claudia.smi;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public abstract class TCloudConstants {
 	
@@ -38,15 +28,6 @@ public abstract class TCloudConstants {
 	public static final String TAG_NETWORK_NETMASK = "Netmask";
 	public static final String TAG_NETWORK_BASE_ADDRESS = "BaseAddress";
 	
-	public static final String TAG_NETWORK_IPLEASES = "IPLease";
-	public static final String TAG_NETWORK_IP = "IP";
-	public static final String TAG_NETWORK_MAC = "MAC";
-	public static final String TAG_NETWORK_MAC_ENABLED = "MacEnabled";
-	public static final String TAG_NETWORK_GATEWAY = "Gateway";
-	public static final String TAG_NETWORK_DNS = "DNS";
-	
-	
-	
 	
 	public static final String TAG_INSTANTIATE_OVF = "InstantiateOvfParams";
 	public static final String ATTR_INSTANTIATE_OVF_NAME = "name";
@@ -54,8 +35,6 @@ public abstract class TCloudConstants {
 	
 	public static final String TAG_INSTANTIATION_PARAMS = "InstantiationParams";
 	public static final String ATTR_INSTANTIATION_PARAMS_NAME = "name";
-	
-	public static final String ATTR_NETWORK_TYPE = "NetworkType";
 	
 	
 	public static final String TAG_NETWORK_CONFIG = "NetworkConfig";
@@ -75,6 +54,49 @@ public abstract class TCloudConstants {
 	public static final String TAG_TASKS="Tasks";
 	public static final String TAG_TASK="Task";
 	
+	public static final String TAG_ENVELOPE = "Envelope";
+		
+	public static final String NAMESPACE_TCLOUD="http://schemas.tcloud.telefonica.com/tcloud/0.1";
+	public static final String NAMESPACE_OVF="http://schemas.dmtf.org/ovf/envelope/1";
+	public static final String NAMESPACE_RASD="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
+	public static final String NAMESPACE_IEP="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_IPProtocolEndpoint";
+	public static final String NAMESPACE_VSSD="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
+	
+	public static final String DEFAULT_STORAGE_UNIT="bytes * 2 ^ 20";
+	
 	public static enum StateType {initial, deploying, active, running, poweredOff, suspended, unactive, error, unknown};
 	
+	public static enum ErrorType {UNKNOWN_ELEMENTS,ELEMENT_NOT_FOUND};
+	
+	public static String[] aggregatedMeasureDescriptors = new String[]{"cpuUsage", "diskUsage", "memoryUsage"};
+	
+	public static Document createErrorMessage(String uri, String elementType, ErrorType errorType) {
+        try {
+			DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder docBuilder;
+
+			docBuilder = dbfac.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
+			
+            Element r = doc.createElement("ErrorSet");
+            doc.appendChild(r);
+            
+            if (errorType==ErrorType.UNKNOWN_ELEMENTS) {
+                Element unknown= doc.createElement("UnknownElements");
+                r.appendChild(unknown);
+                
+                Element element= doc.createElement("element");
+                unknown.appendChild(element);
+                
+                element.setAttribute("type", "vapp");
+                element.setAttribute("ref", uri);
+            }
+            
+            return doc;
+        } catch (IllegalArgumentException iae) {
+        	return null;
+        } catch (ParserConfigurationException pe) {
+        	return null;
+		}
+	}
 }
