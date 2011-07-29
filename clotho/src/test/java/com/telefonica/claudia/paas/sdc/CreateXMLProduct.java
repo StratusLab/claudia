a271b7ceed513490ad90e6498ee1fe82651dd16d
unknown <henar@.hi.inet>
2011-07-29 11:18:32 +0200

    test for paas

---
+++ clotho/src/test/java/com/telefonica/claudia/paas/sdc/CreateXMLProduct.java
@@ -0,0 +1,41 @@
+package com.telefonica.claudia.paas.sdc;
+
+import java.util.HashMap;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+
+import com.telefonica.claudia.slm.deployment.hwItems.Product;
+import com.telefonica.claudia.slm.paas.PaasUtils;
+import com.telefonica.claudia.slm.paas.vmiHandler.SDCClient;
+
+public class CreateXMLProduct {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+		Product product = new Product ("tomcat");
+		product.setProductUrl("http: ");
+		product.setProductVendor("vendor");
+		product.setProductVersion("5.5");
+
+		product.addProperty("propq1", "value1");
+		product.addProperty("propq2", "value2");
+		product.addProperty("propq3", "value3");
+		SDCClient sdc = new SDCClient ("http://dddd:8989/dd");
+		Document doc = null;
+		try {
+			doc = sdc.getInstallProductParams (product,"10.87.67.54");
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println (PaasUtils.tooString(doc));
+
+	}
+
+}
---
+++ clotho/src/test/java/com/telefonica/claudia/paas/sdc/InvokeSDCInstallProduct.java
@@ -0,0 +1,35 @@
+package com.telefonica.claudia.paas.sdc;
+
+import com.telefonica.claudia.slm.deployment.hwItems.Product;
+import com.telefonica.claudia.slm.paas.vmiHandler.SDCClient;
+import com.telefonica.claudia.slm.vmiHandler.exceptions.AccessDeniedException;
+import com.telefonica.claudia.slm.vmiHandler.exceptions.CommunicationErrorException;
+
+public class InvokeSDCInstallProduct {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SDCClient sdc = new SDCClient("http://109.231.82.11:8080/sdc");
+		Product product = new Product ("tomcat");
+		product.setProductUrl("http: ");
+		product.setProductVendor("vendor");
+		product.setProductVersion("5.5");
+
+		product.addProperty("propq1", "value1");
+		product.addProperty("propq2", "value2");
+		product.addProperty("propq3", "value3");
+		try {
+			sdc.installProduct(product, "109.231.82.5");
+		} catch (AccessDeniedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CommunicationErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
