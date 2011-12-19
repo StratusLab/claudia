792f70fd969de7ab36b2af364e099f1f6deebeed
Henar Munoz <henar@tid.es>
2011-12-19 14:34:09 +0100

    remove debian packages to reduce time

--- Deb/createrpm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#!/bin/sh
-echo "------------------------------------------"
-echo "Creando paquetes RPM"
-echo "------------------------------------------"
-#sudo rm *.rpm
-sudo alien -r -c finaldeb/*.deb
-sudo mv *.rpm finalrpm/ 
